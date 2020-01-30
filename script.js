9fac414b3beaf26944372e2c7c8c8814d7d579b5
Kyle Dove <kyledove@Kyles-Air.prymesolutionsgroup.com>
2020-01-29 23:40:12 -0600

    Completed the input for users, added function to store inpout in local storage, get local storage success

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ var getdatetime = document.getElementById("navtime")
 var presentdate = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(presentdate)
 getdatetime.textContent = presentdate
+
+localstore1();
+// localstore2();
+// localstore3();
+// localstore4();
+// localstore5();
+// localstore6();
+// localstore7();
+// localstore8();
+// localstore9();
+
+function localstore1() {
+    var table1 = localStorage.getItem("1")
+    document.getElementById("in9").value = table1
+}
+
+
+
+function submit9() {
+    var event9 = document.getElementById("in9").value;
+    
+    localStorage.setItem("1", event9)
+}
