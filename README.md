24c28168fa76bdf0bd10615b73549d833f9d53d0
Kyle Dove <kyledove@Kyles-MacBook-Air.local>
2020-02-17 10:00:26 -0600

    trouble shooting script.js, corrected errors, when user press submit the input text stores the data in the local browser console log, this can be tested by entering text in the input fields and  refreshing the screen, the outcome should pull the stored input items bacinto the input fields and display the text in the same fields

--- README.md
+++ README.md
@@ -12,23 +12,23 @@ SECTION ONE: SETTING UP THE ENVIORNMENT AND TESTING THE LINKED PAGES
 4.) Add all enviroment variables to read.me for colaberators/contributers (complete)
 5.) Push changes to Master often set timer while in session to push every 45 mins and on exit of the VS IDE
 6.) Add a nav bar that displays the current date and time
-7.) Create a dic that has an ID
+7.) Create a div that has an ID
 8.) link style sheet / test to ensure the style sheet is working
 9.) Test the script.js file by adding the following query to the console: console.log("testing") if the console responds with your quotes text in the console then congratualtions you have linked the script file.
 10.) Test the style.css by adding a dramatic color that can not be missed.
 
 <---------------------------------------------------------------->
 SECTION TWO: ESTABLISH CLEAR GOALS FOR ITIRATION #1
-1.) My first goal is to get the "todays current date and time" from the function that uses moment.js pass the date and time into the div ID created to hold the date and time on the html page
-2.) Add rows for every hour between 9:00 and 5:00 the idea is to display the users working hours
-3.) set the left field input with the "time",
-4.) in the center of the input fields the user can enter "Event Description's",
-5.) on the right die of the input field add "Submit" butons
-6.) allow the user to click save and store the user input into the local browser storage
-7.) Retrive the data from the local storage depsite any refresh or browser end process, test for both conditions
+1.) My first goal is to get the "todays current date and time" from the function that uses moment.js pass the date and time into the div ID created to hold the date and time on the html page //*COMPLETED*//
+2.) Add rows for every hour between 9:00 and 5:00 the idea is to display the users working hours //*COMPLETED*//
+3.) set the left field input with the "time",//*COMPLETED*//
+4.) in the center of the input fields the user can enter "Event Description's",  //*COMPLETED*//
+5.) on the right side of the input field add "Submit" butons //*COMPLETED*//
+6.) allow the user to click save and store the user input into the local browser storage //*COMPLETED*//
+7.) Retrive the data from the local storage depsite any refresh or browser end process, test for both conditions //*COMPLETED*//
 
 Quick Summary of initial creation mile stones:
-The idea is for the My-scheduler app to save the user entry in storage despite any browser end session process or refresh event). My goal is to code the projects first for requirements per the instructions. In the event I have extra time after the functionality is working per the spec provided for the assignment then I will go back and focus on the User Interface/ experience.
+The idea is for the My-scheduler app to save the user entry in storage despite any browser "end session process" or "refresh event". My goal is to code the projects first for requirements per the instructions. In the event I have extra time after the functionality is working per the spec provided for the assignment then I will go back and focus on the User Interface/ experience.
 
 The below items have not been completed by the deadline for the project submission. I will continue to work on them until they are complete.
 <---------------------------------------------------------------->
--- script.js
+++ script.js
@@ -1,30 +1,154 @@
-console.log("script file reporting for class")
-console.log(moment())
+console.log("script file reporting for class");
+console.log(moment());
 
-var getdatetime = document.getElementById("navtime")
-var presentdate = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(presentdate)
-getdatetime.textContent = presentdate
-
-localstore1();
-// localstore2();
-// localstore3();
-// localstore4();
-// localstore5();
-// localstore6();
-// localstore7();
-// localstore8();
-// localstore9();
-
-function localstore1() {
-    var table1 = localStorage.getItem("1")
-    document.getElementById("in9").value = table1
-}
-
-
+var getdatetime = document.getElementById("navtime");
+var presentdate = moment().format("MMMM Do YYYY, h:mm:ss a");
+console.log(presentdate);
+getdatetime.textContent = presentdate;
 
 function submit9() {
-    var event9 = document.getElementById("in9").value;
-    
-    localStorage.setItem("1", event9)
+  var event9 = document.getElementById("in9").value;
+  console.log(event9)
+  localStorage.setItem("1", event9);
+  console.log(localstore1);
 }
+function submit10() {
+  var event10 = document.getElementById("in10").value;
+  localStorage.setItem("2", event10);
+  console.log(localstore2);
+}
+function submit11() {
+  var event11 = document.getElementById("in11").value;
+  localStorage.setItem("3", event11);
+  console.log(localstore3);
+}
+function submit12() {
+  var event12 = document.getElementById("in12").value;
+  localStorage.setItem("4", event12);
+  console.log(localstore4);
+}
+function submit1() {
+  var event1 = document.getElementById("in1").value;
+  console.log(event1);
+  localStorage.setItem("5", event1)
+  console.log(localstore5)
+}
+function submit2() {
+  var event2 = document.getElementById("in2").value;
+  console.log(event2);
+  localStorage.setItem("6", event2);
+  console.log(localstore6);
+}
+function submit3() {
+  var event2 = document.getElementById("in3").value;
+  console.log(event3);
+  localStorage.setItem("7", event3);
+  console.log(localstore7);
+}
+function submit4() {
+  var event2 = document.getElementById("in4").value;
+  console.log(event4);
+  localStorage.setItem("7", event3);
+  console.log(localstore7);
+}
+function submit4() {
+  var event2 = document.getElementById("in5").value;
+  console.log(event5);
+  localStorage.setItem("7", event3);
+  console.log(localstore7);
+}
+
+localstore1();
+localstore2();
+localstore3();
+localstore4();
+localstore5();
+localstore6();
+localstore7();
+localstore8();
+localstore9();
+
+// local storage for 9AM
+function localstore1() {
+  var table1 = localStorage.getItem("1");
+  if (table1 === null) {
+    return;
+  }
+  document.getElementById("in9").value = table1
+
+}
+
+// local storage for 10AM
+function localstore2() {
+  var table2 = localStorage.getItem("2");
+  if (table2 === null) {
+    return;
+  }
+  document.getElementById("in10").value = table2
+}
+
+// local storage for 11AM
+function localstore3() {
+  var table3 = localStorage.getItem("3");
+  if (table3 === null) {
+    return;
+  }
+  document.getElementById("in11").value = table3
+}
+
+// local storage for 12PM
+function localstore4() {
+  var table4 = localStorage.getItem("4");
+  if (table4 === null) {
+    return;
+  }
+  document.getElementById("in12").value = table4
+}
+
+// local storage for 1PM
+function localstore5() {
+  var table5 = localStorage.getItem("5");
+  if (table5 === null) {
+    return;
+  }
+  document.getElementById("in1").value = table5
+}
+
+// local storage for 2PM
+function localstore6() {
+  var table6 = localStorage.getItem("6");
+  if (table6 === null) {
+    return;
+  }
+  document.getElementById("in2").value = table6
+}
+
+// local storage for 3PM
+function localstore7() {
+  var table7 = localStorage.getItem("7");
+  if (table7 === null) {
+    return;
+  }
+  document.getElementById("in3").value = table7
+}
+
+// local storage for 4PM
+function localstore8() {
+  var table8 = localStorage.getItem("8");
+  if (table8 === null) {
+    return;
+  }
+  document.getElementById("in4").value = table8
+}
+
+// local storage for 5PM
+function localstore9() {
+  var table9 = localStorage.getItem("9");
+  if (table9 === null) {
+    return;
+  }
+  document.getElementById("in5").value = table9
+}
+
+
+
