ea0d6e4e24706328fc3f81089b83c20bd3f76b90
Kyle Dove <kyle.dove@epdoffice.com>
2020-03-10 21:03:30 -0500

    Add additional comments
    Updated the js file by adding additional comments. All functions in the project now have a comment above them which describes what they do and the naming convention used for the different variables used.

--- script.js
+++ script.js
@@ -18,52 +18,60 @@ function submit9() {
   localStorage.setItem("1", event9);
   console.log(localstore1);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "10"
 function submit10() {
   var event10 = document.getElementById("in10").value;
   localStorage.setItem("2", event10);
   console.log(localstore2);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "11"
 function submit11() {
   var event11 = document.getElementById("in11").value;
   localStorage.setItem("3", event11);
   console.log(localstore3);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "12"
 function submit12() {
   var event12 = document.getElementById("in12").value;
   localStorage.setItem("4", event12);
   console.log(localstore4);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "1"
 function submit1() {
   var event1 = document.getElementById("in1").value;
   console.log(event1);
   localStorage.setItem("5", event1)
   console.log(localstore5)
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "2"
 function submit2() {
   var event2 = document.getElementById("in2").value;
   console.log(event2);
   localStorage.setItem("6", event2);
   console.log(localstore6);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "3"
 function submit3() {
   var event3 = document.getElementById("in3").value;
   console.log(event3);
   localStorage.setItem("7", event3);
   console.log(localstore7);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "4"
 function submit4() {
   var event4 = document.getElementById("in4").value;
   console.log(event4);
   localStorage.setItem("8", event4);
   console.log(localstore8);
 }
+// Setting value from input field in the lcoal storage when user clicks the "submit" button on the input field with the title "5"
 function submit5() {
   var event5 = document.getElementById("in5").value;
   console.log(event5);
   localStorage.setItem("9", event5);
   console.log(localstore9);
 }
-
+// local storeage tables that store values from the functions
 localstore1();
 localstore2();
 localstore3();
@@ -74,7 +82,7 @@ localstore7();
 localstore8();
 localstore9();
 
-// local storage for 9AM
+// local storage for 9AM, function gets the value from the local sttorage, the naming convention of the variable is consistent with it's use, the function is getting values from a local storeage table which is why the variable is named "table" a number consistent with the actual table number in the local storage is appended to the table for additional context, i.e. table1 is the first table in the local storage if you were to go into the developer tools in the browser wheile this is running you will see the tables are consistent with the naming convention used.
 function localstore1() {
   var table1 = localStorage.getItem("1");
   if (table1 === null) {
